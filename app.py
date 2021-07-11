1f0446f170054678f29cfa579b1dfaf71fb280f7
Mahesh G <47917543+maheshgshno3@users.noreply.github.com>
2021-07-12 00:18:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,24 @@ data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
 
 st.subheader(f'Stock Market Data of {selected_stock}')
+
+tickerData = yf.Ticker(selected_stock) # Get ticker data
+
+# Ticker information
+string_logo = '<img src=%s>' % tickerData.info['logo_url']
+st.markdown(string_logo, unsafe_allow_html=True)
+
+string_name = tickerData.info['longName']
+st.header('**%s**' % string_name)
+
+string_summary = tickerData.info['longBusinessSummary']
+st.info(string_summary)
+
 st.subheader('Raw data')
 'The Complete Stock Data as extracted from Yahoo Finance: '
 data
+
+
 
 # Plot raw data
 
@@ -78,123 +93,123 @@ data["mov_avg_open"] = data['Open'].rolling(
 st.line_chart(data[["mov_avg_open", "Open"]])
 
 
-# def aco():  # activate ACO
-#     iteration = 1000
-#     n_ants = 500000
-#     n_citys = 500000
-#     m = n_ants
-#     n = n_citys
-#     e = .5  # evaporation rate
-#     alpha = 2  # pheromone factor
-#     beta = 1  # visibility factor
+def aco():  # activate ACO
+    iteration = 1000
+    n_ants = 500000
+    n_citys = 500000
+    m = n_ants
+    n = n_citys
+    e = .5  # evaporation rate
+    alpha = 2  # pheromone factor
+    beta = 1  # visibility factor
 
-#     # calculating the visibility of the next city visibility(i,j)=1/d(i,j)
+    # calculating the visibility of the next city visibility(i,j)=1/d(i,j)
 
-#     visibility = 1/d
-#     visibility[visibility == inf] = 0
+    visibility = 1/d
+    visibility[visibility == inf] = 0
 
-#     # intializing pheromne present at the paths to the cities
+    # intializing pheromne present at the paths to the cities
 
-#     pheromne = .1*np.ones((m.StockName, n.StockName.dates))
+    pheromne = .1*np.ones((m.StockName, n.StockName.dates))
 
-#     # intializing the rute of the ants with size rute(n_ants,n_citys+1)
-#     # note adding 1 because we want to come back to the source city
+    # intializing the rute of the ants with size rute(n_ants,n_citys+1)
+    # note adding 1 because we want to come back to the source city
 
-#     rute = np.ones((m.StockName, n.StockName+1))
+    rute = np.ones((m.StockName, n.StockName+1))
 
-#     for ite in range(iteration):
+    for ite in range(iteration):
 
-#         # initial starting and ending positon of every ants '1' i.e city '1'
-#         rute[:, 0] = 1
+        # initial starting and ending positon of every ants '1' i.e city '1'
+        rute[:, 0] = 1
 
-#         for i in range(m.StockName):
+        for i in range(m.StockName):
 
-#             # creating a copy of visibility
-#             temp_visibility = np.array(visibility)
+            # creating a copy of visibility
+            temp_visibility = np.array(visibility)
 
-#             for j in range(n.StockName-1):
-#                 # print(rute)
+            for j in range(n.StockName-1):
+                # print(rute)
 
-#                 # intializing combine_feature array to zero
-#                 combine_feature = np.zeros(5)
-#                 # intializing cummulative probability array to zeros
-#                 cum_prob = np.zeros(5)
+                # intializing combine_feature array to zero
+                combine_feature = np.zeros(5)
+                # intializing cummulative probability array to zeros
+                cum_prob = np.zeros(5)
 
-#                 cur_loc = int(rute[i, j]-1)  # current city of the ant
+                cur_loc = int(rute[i, j]-1)  # current city of the ant
 
-#                 # making visibility of the current city as zero
-#                 temp_visibility[:, cur_loc] = 0
+                # making visibility of the current city as zero
+                temp_visibility[:, cur_loc] = 0
 
-#                 # calculating pheromne feature
-#                 p_feature = np.power(pheromne[cur_loc, :], beta)
-#                 # calculating visibility feature
-#                 v_feature = np.power(temp_visibility[cur_loc, :], alpha)
+                # calculating pheromne feature
+                p_feature = np.power(pheromne[cur_loc, :], beta)
+                # calculating visibility feature
+                v_feature = np.power(temp_visibility[cur_loc, :], alpha)
 
-#                 # adding axis to make a size[5,1]
-#                 p_feature = p_feature[:, np.newaxis]
-#                 # adding axis to make a size[5,1]
-#                 v_feature = v_feature[:, np.newaxis]
+                # adding axis to make a size[5,1]
+                p_feature = p_feature[:, np.newaxis]
+                # adding axis to make a size[5,1]
+                v_feature = v_feature[:, np.newaxis]
 
-#                 # calculating the combine feature
-#                 combine_feature = np.multiply(p_feature, v_feature)
+                # calculating the combine feature
+                combine_feature = np.multiply(p_feature, v_feature)
 
-#                 total = np.sum(combine_feature)  # sum of all the feature
+                total = np.sum(combine_feature)  # sum of all the feature
 
-#                 # finding probability of element probs(i) = combine_feature(i)/total
-#                 probs = combine_feature/total
+                # finding probability of element probs(i) = combine_feature(i)/total
+                probs = combine_feature/total
 
-#                 cum_prob = np.cumsum(probs)  # calculating cummulative sum
-#                 # print(cum_prob)
-#                 r = np.random.random_sample()  # random no in [0,1)
-#                 # print(r)
-#                 # finding the next city having probability higher then random(r)
-#                 city = np.nonzero(cum_prob > r)[0][0]+1
-#                 # print(city)
+                cum_prob = np.cumsum(probs)  # calculating cummulative sum
+                # print(cum_prob)
+                r = np.random.random_sample()  # random no in [0,1)
+                # print(r)
+                # finding the next city having probability higher then random(r)
+                city = np.nonzero(cum_prob > r)[0][0]+1
+                # print(city)
 
-#                 rute[i, j+1] = city  # adding city to route
+                rute[i, j+1] = city  # adding city to route
 
-#             # finding the last untraversed city to route
-#             left = list(
-#                 set([i for i in range(1, n.StockName+1)])-set(rute[i, :-2]))[0]
+            # finding the last untraversed city to route
+            left = list(
+                set([i for i in range(1, n.StockName+1)])-set(rute[i, :-2]))[0]
 
-#             rute[i, -2] = left  # adding untraversed city to route
+            rute[i, -2] = left  # adding untraversed city to route
 
-#         rute_opt = np.array(rute)  # intializing optimal route
+        rute_opt = np.array(rute)  # intializing optimal route
 
-#         # intializing total_distance_of_tour with zero
-#         dist_cost = np.zeros((m, 1))
-#         # finding location of minimum of dist_cost
-#         dist_min_loc = np.argmin(dist_cost)
-#         dist_min_cost = dist_cost[dist_min_loc]  # finging min of dist_cost
+        # intializing total_distance_of_tour with zero
+        dist_cost = np.zeros((m, 1))
+        # finding location of minimum of dist_cost
+        dist_min_loc = np.argmin(dist_cost)
+        dist_min_cost = dist_cost[dist_min_loc]  # finging min of dist_cost
 
-#         # intializing current traversed as best route
-#         best_route = rute[dist_min_loc, :]
-#         pheromne = (1-e)*pheromne  # evaporation of pheromne with (1-e)
+        # intializing current traversed as best route
+        best_route = rute[dist_min_loc, :]
+        pheromne = (1-e)*pheromne  # evaporation of pheromne with (1-e)
 
-#         for i in range(m):
+        for i in range(m):
 
-#             s = 0
-#             for j in range(n-1):
+            s = 0
+            for j in range(n-1):
 
-#                 # calcualting total tour distance
-#                 s = s + d[int(rute_opt[i, j])-1, int(rute_opt[i, j+1])-1]
+                # calcualting total tour distance
+                s = s + d[int(rute_opt[i, j])-1, int(rute_opt[i, j+1])-1]
 
-#             # storing distance of tour for 'i'th ant at location 'i'
-#             dist_cost[i] = s
-#             for i in range(m):
-#                 for j in range(n-1):
-#                     dt = 1/dist_cost[i]
-#                     pheromne[int(rute_opt[i, j])-1,
-#                              int(rute_opt[i, j+1])-1] += dt
-#                     if dt > pheromne:
-#                         max = stocker(rute.pheromne)
-#                     # updating the pheromne with delta_distance
-#                     # delta_distance will be more with min_dist i.e adding more weight to that route peromne
+            # storing distance of tour for 'i'th ant at location 'i'
+            dist_cost[i] = s
+            for i in range(m):
+                for j in range(n-1):
+                    dt = 1/dist_cost[i]
+                    pheromne[int(rute_opt[i, j])-1,
+                             int(rute_opt[i, j+1])-1] += dt
+                    if dt > pheromne:
+                        max = stocker(rute.pheromne)
+                    # updating the pheromne with delta_distance
+                    # delta_distance will be more with min_dist i.e adding more weight to that route peromne
 
 
-# def predict():
-#     m.StockName = aco.max.pheromne  # Upper Limit
-#     m2, m2.Stockname = aco.max.pheromne
+def predict():
+    m.StockName = aco.max.pheromne  # Upper Limit
+    m2, m2.Stockname = aco.max.pheromne
 
 
 # Predict forecast with Prophet.
